ff6761317eb16890dc21c017612d80c23f7302db
diuere <86838002+diuere@users.noreply.github.com>
2022-10-30 19:48:43 -0300

    implementing controlled form
    * new state object to store the input values and allow react to control them was created;
    * onChange function created and implemented on each contact inputs;
    * react can controll all inputs state.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,34 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function ContactForm() {
-    const form = useRef();
-
-    const sendEmail = (e) => { // sending emails using EmailJS
-      e.preventDefault();
+  const form = useRef();
   
-      // emailjs.sendForm('service_7mfuzbo', 'template_ob9k3p8', form.current, 'OjtO9_H9y03jj1H36')
-      emailjs.sendForm('service_e8tjba3', 'template_bhovy0m', form.current, 'J-tFMwV5sJF45Q8em')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+  const [contactInputs, setContactInputs] = useState(
+    {
+      from_name: "",
+      from_email: "",
+      message: "",
+    }
+  )
 
-      e.target.reset();
-    };
+  const sendEmail = (e) => { // sending emails using EmailJS
+    e.preventDefault();
+
+    // emailjs.sendForm('service_7mfuzbo', 'template_ob9k3p8', form.current, 'OjtO9_H9y03jj1H36')
+    emailjs.sendForm('service_e8tjba3', 'template_bhovy0m', form.current, 'J-tFMwV5sJF45Q8em')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setContactInputs(state => (
+      {
+        ...state,
+        [name]: value,
+      }
+    ))
+  }
 
   return (
     <form ref={form} onSubmit={sendEmail}>
         <label htmlFor="userName">Name</label>
-        <input type="text" name="from_name" id="userName" required/>
+        <input type="text" name="from_name" value={contactInputs.from_name} id="userName" required onChange={(e) => handleInputChange(e)}/>
 
         <label htmlFor="userEmail">Email</label>
-        <input type="email" name="from_email"  id="userEmail" required/>
+        <input type="email" name="from_email" value={contactInputs.from_email} id="userEmail" required onChange={(e) => handleInputChange(e)}/>
         
         <label htmlFor="contactMessage">Message</label>
-        <textarea name="message" id="contactMessage" cols="30" rows="10" required></textarea>
-        
+        <textarea name="message" id="contactMessage" value={contactInputs.message} cols="30" rows="10" required onChange={(e) => handleInputChange(e)}></textarea>
         
         <button className="main-btn-style">submit</button>
     </form>
