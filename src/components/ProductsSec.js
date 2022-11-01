ddb8e07ef6b717154f992f8352af73a0a49c4428
diuere <86838002+diuere@users.noreply.github.com>
2022-11-01 14:24:50 -0300

    new intersection observer implemented
    >> * new keyframe created;
    >> * the button, now classified as .review-cocktails-btn that leads to the cocktail page received an animation effect;
    >> * new observer that toggles the animation was created;
    >> * .review-cocktails-btn has an 'emphasizing effec' whenever it's intersecting.

--- src/components/ProductsSec.js
+++ src/components/ProductsSec.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
+const reviewObserver = () => {
+  const reviewCocktailsBtn = document.querySelector(".review-cocktails-btn");
+
+  const reviewObserver = new IntersectionObserver((entries, reviewObserver) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        reviewCocktailsBtn.classList.add("emphasizing");
+      }
+    })
+  }, {})
+
+  reviewObserver.observe(reviewCocktailsBtn);
+}
+
 export default function ProductsSec() {
+  useEffect(() => {
+    reviewObserver();
+  }, [])
+
   return (
     <>
       <div className="products-preview-desc">
         <h2>Searching for cocktails just became much easier</h2>
         <p>If your desire is to taste the world's greatest cocktails, we've got your back.</p>
-        <Link to="/productsComponent/products" aria-label="review cocktails">Review</Link>
+        <Link to="/productsComponent/products" aria-label="review cocktails" className="review-cocktails-btn">Review</Link>
       </div>
       <div className="products-demo">
         <img src="https://www.thecocktaildb.com/images/media/drink/8kxbvq1504371462.jpg" alt="cocktail demonstration" />
